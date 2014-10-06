cd336e4f9a246bfde57e7dd645cc14d882c58afc
Ian Propper <ipropper@umich.edu>
2014-10-05 23:29:36 -0400

    added basic desktop app, on running it will mute your computers volume. Note*** User must have Nir Soft installed

---
+++ DesktopAppFolder/ConsoleAppMsChallenge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //creating a new process
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+            // makes command line window hidden
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+
+            //calling process name
+            startInfo.FileName = "cmd.exe";
+
+            //establish that I want to use the console as an output stream (receives input from my output)s
+            startInfo.RedirectStandardInput = true;
+            startInfo.UseShellExecute = false;
+
+            process.StartInfo = startInfo;
+            process.Start();
+
+            StreamWriter cmd_writer = process.StandardInput;
+
+            // command to mute the volume
+            string cmd_output = "nircmd.exe mutesysvolume 1";
+
+
+            cmd_writer.WriteLine(cmd_output);
+        }
+    }
+}
