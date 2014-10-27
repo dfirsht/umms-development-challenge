755b6ecfc32811ae0ead8e90ad03fea5bb1fc479
Aaron Barber <asbarber@umich.edu>
2014-10-27 14:50:08 -0400

    Added functions to manage computer settings and power state (opening control panel, putting the PC to sleep).  Added functions to run applications (open a web browser, open microsoft word).

--- DesktopAppFolder/ConsoleAppMsChallenge/Controller.cs
+++ DesktopAppFolder/ConsoleAppMsChallenge/Controller.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using System.Drawing;
-using System.Windows.Input;
 
 namespace CMD
 {
@@ -158,8 +153,7 @@ namespace CMD
         {
             /**
              * Sample constant to send the <Enter> Key
-             */
-            
+             */   
             public static String ENTER = "{ENTER}";
 
 
@@ -184,5 +178,200 @@ namespace CMD
 
            
        }
+
+        /**
+         * Utility for settings management of the computer.
+         * 
+         * For example, opening the control panel or shutting down the PC.
+         * 
+         * Does NOT require a form handle.
+         */
+        public class SettingsControl
+        {
+            /**
+             * Imported function to suspend (sleep or hibernate) the computer.
+             */
+            [DllImport("user32")]
+            public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+            /**
+             * Imported function to lock the computer.
+             */
+            [DllImport("user32")]
+            public static extern void LockWorkStation();
+
+            /**
+             * Opens the control panel as a new process.
+             */
+            public static void openControlPanel()
+            {
+                Process.Start("control");
+            }
+
+            /**
+             * Opens the task manager as a new process.
+             */
+            public static void openTaskManager()
+            {
+                Process.Start("taskmgr");
+            }
+
+            /**
+             * Shuts off the computer.
+             */
+            public static void pcShutdown()
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+
+            /**
+             * Restarts the computer.
+             */
+            public static void pcRestart()
+            {
+                Process.Start("shutdown", "/r /t 0");
+            }
+
+            /**
+             * Logs off the current user of the computer.
+             */
+            public static void pcLogOff()
+            {
+                ExitWindowsEx(0, 0);
+            }
+
+            /**
+             * Locks the computer.
+             */
+            public static void pcLock()
+            {
+                LockWorkStation();
+            }
+
+            /**
+             * Hibernates the computer.
+             */
+            public static void pcHibernate()
+            {
+                Application.SetSuspendState(PowerState.Hibernate, true, true);
+            }
+
+            /**
+             * Puts the computer in standby (sleep).
+             */
+            public static void pcStandby()
+            {
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+            }
+        }
+
+        /**
+         * Utility for interacting with applications.
+         * 
+         * Does NOT require a form handle.
+         */
+        public class ApplicationControl
+        {
+            //Applications that can be opened
+            //---------------------------------------------------------------------
+            //Common Programs
+            public static String appNotepad = "notepad.exe";
+            public static String appItunes = "itunes.exe";
+            public static String appSteam = "steam.exe";
+
+            //Standard Windows Programs
+            public static String appInternetExplorer = "iexplore.exe";
+            public static String appFileExplorer = "explorer.exe";
+            public static String appWebBrowser = "http://www.microsoft.com";
+            public static String appWebGoogle = "http://www.google.com";
+
+            //Microsoft Office
+            public static String appMSWord = "winword.exe";
+            public static String appMSPowerPoint = "powerpnt.exe";
+            public static String appMSExcel = "excel.exe";
+            //---------------------------------------------------------------------
+
+            //Useful Keywords
+            //---------------------------------------------------------------------
+            public static String appWebSearch = "http://google.com/serach?q=";
+            //---------------------------------------------------------------------
+
+            //Command line arguments associated with applications
+            //---------------------------------------------------------------------
+            public static String appSteam_argBigPicture = "-bigpicture";
+            //---------------------------------------------------------------------
+
+            /**
+             * Opens the program from the specified path.
+             * Will return true if the open was successful,
+             * false otherwise.
+             * 
+             * @param path: file location or file name
+             */
+            public static bool openProgram(String path)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = path;
+                return openProcess(info);
+            }
+
+            /**
+             * Opens the program from the specified path with the given command
+             * line arguments.
+             * Will return true if the open was successful,
+             * false otherwise.
+             * 
+             * @param path: file location or file name
+             * @param args: command line arguments of the program
+             */
+            public static bool openProgram(String path, String args)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = path;
+                info.Arguments = args;
+                return openProcess(info);
+            }
+
+            /**
+             * Runs the process, and returns true if successful, otherwise false.
+             * The process could be opening an application (Microsoft Word) or
+             * opening a file (bankStatement.docx).
+             * 
+             * @param info: start information for the process, such as
+             *              the file path or command line arguments
+             */
+            public static bool openProcess(ProcessStartInfo info)
+            {
+                try
+                {
+                    Process.Start(info);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+
+            /**
+             * Searches google with the given query.
+             * Returns true if successful, otherwise false.
+             * 
+             * @param query: google query phrase to search for 
+             *               example: "Why is the sky blue?"
+             */
+            public static bool searchGoogle(string query)
+            {
+                try
+                {
+                    Process.Start(appWebSearch + query);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
 
 }
