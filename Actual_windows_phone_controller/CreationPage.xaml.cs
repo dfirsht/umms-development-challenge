f532b91fa6d7b8bb2e781fa0da0414de9b79f169
Daniel Firsht <dfirsht@gmail.com>
2014-11-06 15:45:37 -0500

    added basic menu bar to creator

--- Actual_windows_phone_controller/CreationPage.xaml.cs
+++ Actual_windows_phone_controller/CreationPage.xaml.cs
@@ -15,37 +15,58 @@ namespace Actual_windows_phone_controller
 {
     public partial class CreationPage : PhoneApplicationPage
     {
-        private TranslateTransform move = new TranslateTransform();
-        private ScaleTransform resize = new ScaleTransform();
-        private TransformGroup rectangleTransforms = new TransformGroup();
-
         public CreationPage()
         {
             InitializeComponent();
-            // Combine the moving and resizing tranforms into one TransformGroup.
-            // The rectangle's RenderTransform can only contain a single transform or TransformGroup.
-            rectangleTransforms.Children.Add(move);
-            rectangleTransforms.Children.Add(resize);
-            rectangle.RenderTransform = rectangleTransforms;
-
-            // Handle manipulation events.
-            rectangle.ManipulationDelta +=
-                new EventHandler<ManipulationDeltaEventArgs>(Rectangle_ManipulationDelta);
         }
 
-        void Rectangle_ManipulationDelta(object sender, ManipulationDeltaEventArgs e)
+        private void toolbarItemSelected(object sender, MouseButtonEventArgs e)
         {
-            // Move the rectangle.
-            move.X += e.DeltaManipulation.Translation.X;
-            move.Y += e.DeltaManipulation.Translation.Y;
+            // Inialize new object
+            Rectangle newRectangle = new Rectangle();
+            newRectangle.Width = 100;
+            newRectangle.Height = 100;
+            newRectangle.Fill = new SolidColorBrush(Colors.Blue);
+            Point mouseCordinates = e.GetPosition(controllerCanvas);
+            //Set object position
+            Canvas.SetLeft(newRectangle, mouseCordinates.X - newRectangle.Width / 2);
+            Canvas.SetTop(newRectangle, mouseCordinates.Y - newRectangle.Height / 2);
+            controllerCanvas.Children.Add(newRectangle);
+            //Set event handlers
+            mousePreviousPosition = mouseCordinates;
+            newRectangle.MouseMove += changePosition;
+            newRectangle.ManipulationDelta += changeSize;
+            newRectangle.MouseLeftButtonDown += recordPosition;
+            newRectangle.CaptureMouse();
+        }
 
-            // Resize the rectangle.
+        private void changePosition(object sender, MouseEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(controllerCanvas);
+            double originalLeft = Canvas.GetLeft((UIElement)sender);
+            double originalTop = Canvas.GetTop((UIElement)sender);
+            Canvas.SetLeft((UIElement)sender, originalLeft + mousePosition.X - mousePreviousPosition.X);
+            Canvas.SetTop((UIElement)sender, originalTop + mousePosition.Y - mousePreviousPosition.Y);
+            mousePreviousPosition = mousePosition;
+        }
+
+        private void changeSize(object sender, ManipulationDeltaEventArgs e)
+        {
             if (e.DeltaManipulation.Scale.X > 0 && e.DeltaManipulation.Scale.Y > 0)
             {
-                // Scale the rectangle.
-                resize.ScaleX *= e.DeltaManipulation.Scale.X;
-                resize.ScaleY *= e.DeltaManipulation.Scale.Y;
+                FrameworkElement rectangle = (FrameworkElement)sender;
+                double centerX = Canvas.GetLeft(rectangle) + rectangle.Width / 2;
+                double centerY = Canvas.GetTop(rectangle) + rectangle.Height / 2;
+                rectangle.Width = rectangle.Width * e.DeltaManipulation.Scale.X;
+                rectangle.Height = rectangle.Height * e.DeltaManipulation.Scale.Y;
+                Canvas.SetLeft(rectangle, centerX - rectangle.Width  / 2);
+                Canvas.SetTop(rectangle, centerY - rectangle.Height / 2);
             }
+        }
+        Point mousePreviousPosition;
+        private void recordPosition(object sender, MouseButtonEventArgs e)
+        {
+            mousePreviousPosition = e.GetPosition(controllerCanvas);
         }
     }
 }
